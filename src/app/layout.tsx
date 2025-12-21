5950dfefae25b3b8d756580a5ead0099ad79b3a5
ggcweb <garggcwebsites@gmail.com>
2025-12-21 00:16:04 -0500

    stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://crawfordnotchcamping.com'),
   title: {
     template: '%s | Crawford Notch Campground',
     default: 'Crawford Notch General Store & Campground',
